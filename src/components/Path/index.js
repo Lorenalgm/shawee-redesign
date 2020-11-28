13d7f298b26634c68edb74cf1ee39c6122d47e0b
Lorenalgm <lorena@proesc.com>
2020-11-28 18:55:09 -0300

    Criado jsx da tela de hackathon

--- src/components/Path/index.js
+++ src/components/Path/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 
-function Path({props}){
+function Path(props){
     return(
         <div className="path-container">
            <div className="path-name">
--- src/pages/Hackathon/index.js
+++ src/pages/Hackathon/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../components/Header';
 import Menu from '../../components/Menu';
 import Path from '../../components/Path';
+import Indicator from '../../components/Indicator';
 import './styles.css';
 import { Link } from 'react-router-dom';
 
@@ -13,14 +14,15 @@ function Hachathon(){
                 <Menu />
                 <div className="content">
                     <div className="path">
-                        <p>xxxxxxx</p>
                         <Path order="1" title="Escolha seu desafio" status="Concluído" deadline_date="08/11" completed_date="08/11" />
                         <Path order="2" title="Escolha seu time" status="Pendente" deadline_date="10/11" />
                         <Path order="3" title="Entregar checkpoint" status="Pendente" deadline_date="13/11" />
                         <Path order="4" title="Submeter projeto" status="Pendente" deadline_date="14/11" />
                     </div>
                     <div className="indicators">
-                      
+                        <Indicator title="Atividades concluídas" number="1" total="4" />
+                        <Indicator title="Mentorias participadas" number="1" />
+                        <Indicator title="Pontos acumulados" number="250" />
                     </div>
                 </div> 
             </div>
