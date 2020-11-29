f6c7364e5e7bb4facc56196ae98161d6cf0df64a
Lorenalgm <lorena@proesc.com>
2020-11-29 20:29:29 -0300

    Estilizada página de desafiantes

--- src/pages/Challenges/index.js
+++ src/pages/Challenges/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Header from '../../components/Header';
 import MenuHacka from '../../components/MenuHacka';
+import Challenge from '../../components/Challenge';
 import './styles.css';
+import sebrae from '../../assets/sebrae.png';
 
 function Challenges(){
     return(
@@ -12,6 +14,17 @@ function Challenges(){
                 <div className="challenges-content">
                     <h1>Desafios</h1>
                     <div className="challenges">
+                        <Challenge title="Sebrae" description="Como a atuação em rede pode ajudar a solucionar problemas escancarados
+                        pela crise da COVID-19: gestão do tempo; limites entre profissional e pessoal; cuidados
+                        com crianças/ idosos; saúde psicológica e violência doméstica?" image_url={sebrae} />
+                        
+                        <Challenge title="Sebrae" description="Como a atuação em rede pode ajudar a solucionar problemas escancarados
+                        pela crise da COVID-19: gestão do tempo; limites entre profissional e pessoal; cuidados
+                        com crianças/ idosos; saúde psicológica e violência doméstica?" image_url={sebrae} />
+                         
+                        <Challenge title="Sebrae" description="Como a atuação em rede pode ajudar a solucionar problemas escancarados
+                        pela crise da COVID-19: gestão do tempo; limites entre profissional e pessoal; cuidados
+                        com crianças/ idosos; saúde psicológica e violência doméstica?" image_url={sebrae} />
                     </div>
                 </div> 
             </div>
